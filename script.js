a8ad1d94d3eb3854edfd470d56e4df11f8ca23f4
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-06-14 13:07:45 +0530

    Created script.js file and added the functions for calculation

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function remainder(num1, num2) {
+  return num1 % num2;
+}
+
+function power(num1, num2) {
+  return num1 ** num2;
+}
